29a77d3a43d8010ab8ce51fccacc1c3db9b14074
KibaZhenSun <kibazhensun@gmail.com>
2015-05-29 12:45:19 -0400

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Kiba on 5/29/15.
+//  Copyright (c) 2015 Kiba Zhen Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Kiba on 5/29/15.
+//  Copyright (c) 2015 Kiba Zhen Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
